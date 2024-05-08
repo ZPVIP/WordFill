dd8c3361de3eff76384382c6e2d993157d461100
Peter <zpregister@gmail.com>
2024-05-08 05:34:18 +0000

    Added button for hint and sound

--- src/WordFill.js
+++ src/WordFill.js
@@ -5,6 +5,8 @@ function WordFill() {
     const [word, setWord] = useState({ incomplete_word: '', complete_word: '', context: '' });
     const [wrong, setWrong] = useState('');
     const [completeWord, setCompleteWord] = useState('');
+    const [showHint, setShowHint] = useState(false);
+    const [playSound, setPlaySound] = useState(true);
     const [inputs, setInputs] = useState([]);
     const [currentInputIndex, setCurrentInputIndex] = useState(0);
     const inputRefs = inputs.map(() => createRef());
@@ -12,6 +14,9 @@ function WordFill() {
     const [isRandom, setIsRandom] = useState(false);
     const [currentIndex, setCurrentIndex] = useState(0);
     const words = all_words.words;
+    const audioTyping = new Audio('/sounds/typing.mp3');
+    const audioRight = new Audio('/sounds/right.mp3');
+    const audioError = new Audio('/sounds/error.mp3');
 
     useEffect(() => {
         fetchWord();
@@ -29,13 +34,12 @@ function WordFill() {
 
             setWord(data);
             setInputs(new Array(data.complete_word.length - data.incomplete_word.length).fill(''));
+            setShowHint(false);
             setCurrentInputIndex(0);
         } catch (error) {
             console.error('Failed to fetch word:', error);
         }
     };
-
-
 
     const handleInputChange = (e, index) => {
         const newInputs = [...inputs];
@@ -48,25 +52,28 @@ function WordFill() {
         // If the cursor is not at the last input, focus on the next input
         if (index === inputs.length - 1) {
             if (newInputs.join('') === word.complete_word.slice(word.incomplete_word.length)) {
+                playSound && audioRight.play();
                 fetchWord();
                 setWrong('');
                 setCompleteWord('');
                 inputRefs[0].current.focus();
             } else {
+                playSound && audioError.play();
                 setInputs(new Array(word.complete_word.length - word.incomplete_word.length).fill(''));
                 inputRefs[0].current.focus();
                 // 在input 后面加上一个红色的 x, 表示答案错误
                 setWrong('x');
             }
         } else if (e.target.value !== '') {
+            playSound && audioTyping.play();
             setCurrentInputIndex(index + 1);
             inputRefs[index + 1].current.focus();
         }
     };
 
     const handleKeyDown = (e) => {
-        if (e.ctrlKey && e.key === 'a') {
-            setCompleteWord(word.complete_word);
+        if (e.ctrlKey && e.key === 'h') {
+            setShowHint(!showHint);
         }
         // If the user press backspace, delete current input and focus on the previous input
         if (e.key === 'Backspace') {
@@ -80,13 +87,33 @@ function WordFill() {
         }
     };
 
+    const toggleHint = () => {
+        setShowHint(!showHint);
+    };
+
+    const toggleSound = () => {
+        setPlaySound(!playSound);
+    };
+
     return (
         <div>
             <div style={{position: 'absolute', top: '10px', right: '10px'}}>
-                <input type="checkbox" checked={isRandom} onChange={() => setIsRandom(!isRandom)} /> Random
+                <button onClick={toggleHint} style={{marginRight: '10px'}}>
+                    {showHint ? 'Hide Hint' : 'Show Hint'}
+                </button>
+                <button onClick={toggleSound} style={{marginRight: '10px'}}>
+                    {playSound ? 'Mute Sound' : 'Play Sound'}
+                </button>
+                <input type="checkbox" checked={isRandom} onChange={() => setIsRandom(!isRandom)}/> Random
             </div>
-            <div style={{ textAlign: 'center', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-                <div style={{ display: 'flex', fontSize: '3em', justifyContent: 'center', alignItems: 'center', flexWrap: 'wrap' }}>
+            <div style={{
+                textAlign: 'center',
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)'
+            }}>
+            <div className="typingInput" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexWrap: 'wrap', flexDirection: 'row' }}>
                     <span>{word.context.split('@@')[0]} &nbsp;</span>
                     <span>{word.incomplete_word}</span>
                     {inputs.map((value, index) => (
@@ -97,16 +124,17 @@ function WordFill() {
                             value={value}
                             onChange={(e) => handleInputChange(e, index)}
                             onKeyDown={handleKeyDown}
-                            style={{width: '60px', height: '80px', textAlign: 'center', fontSize: '1em', margin: '0 5px', border: '1px solid #000'}}
+                            style={{width: '0.9em', height: '1.2em', textAlign: 'center', fontSize: '1em', margin: '0.08em', borderBottom: '1px solid #000', borderLeft: '0px', borderRight: '0px', borderTop: '0px', outline: 'none'}}
                         />
                     ))}
                     <span>&nbsp;{word.context.split('@@')[1]}</span>
                     <span style={{color: 'red', padding: '0 0 0 20px'}}>{wrong}</span>
                 </div>
-                <div>&nbsp;</div>
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <span>{completeWord}</span>
-                </div>
+                {showHint && (
+                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '20px' }}>
+                        <span>{word.complete_word}</span>
+                    </div>
+                )}
             </div>
         </div>
     );
